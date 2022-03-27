ae9ef4d98c3a11b0cfa5e001d648e16968a2c215
vimodkk <63736570+vimodkk@users.noreply.github.com>
2022-03-27 16:25:12 +0530

    Add files via upload

---
+++ styles/Wayanad_panchayat_4_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Wayanad_panchayat_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 1.5;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("PANCHAYAT") !== null) {
+        labelText = String(feature.get("PANCHAYAT"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(106,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
